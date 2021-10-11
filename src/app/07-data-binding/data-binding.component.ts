bfc09903ad7f5c4d7f7a9b82e78f400dab45c323
Dorivan Sousa <dorivansousa@hotmail.com>
2021-10-11 08:32:31 -0300

    Event binding

--- src/app/07-data-binding/data-binding.component.ts
+++ src/app/07-data-binding/data-binding.component.ts
@@ -12,6 +12,10 @@ export class DataBindingComponent implements OnInit {
   urlImage: String = 'http://lorempixel.com/100/100/nature';
   tagImageInterpolation = 'HTML <img src="{{ urlImage }}" alt="">';
   tagImagePropertyBinding = 'HTML <img [src]="urlImage" alt="">';
+  inputValue: String = '';
+  inputValueSave: String = '';
+
+  isMouseOver: boolean = false;
 
   constructor() { 
     this.url = "http:\\loiane.com";
@@ -23,6 +27,23 @@ export class DataBindingComponent implements OnInit {
 
   getCursoAngular() {
     return false
+  }
+
+  onButtonClick () {
+    alert('oi');
+  }
+
+  onInputKeyUp(event: KeyboardEvent) {
+   this.inputValue = (<HTMLInputElement>event.target).value;
+  }
+
+  onInputKeyUpEnter(value: any) {
+    this.inputValueSave = value;
+
+  }
+
+  onMouseOverOut() {
+    this.isMouseOver = !this.isMouseOver;
   }
 
   ngOnInit(): void {
